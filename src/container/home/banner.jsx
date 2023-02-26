7fd0fe6dd4bc26572a980ed9bd7e544c4221bed1
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-02-26 19:35:36 +0900

    banner text 수정

--- src/container/home/banner.jsx
+++ src/container/home/banner.jsx
@@ -8,12 +8,12 @@ const Banner = () => {
   return (
     <section className="relative w-full flex flex-col items-center">
       <p className="absolute top-80 text-center z-10 font-bold text-7xl text-white break-keep">
-        WANNABE 개발계의 AOMG
+        실전 프로젝트를 통해 성장 기회와 프로로써의 경험을 얻을 수 있는 단체
       </p>
       <img
         className="bg-center  bg-cover w-full h-full align-middle  bg-no-repeat"
         src={DataUrl}
-        alt=''
+        alt=""
       />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center overflow-auto">
         <span className="break-all mb-8 text-white text-6xl font-bold"></span>
