ac36a0f36a57c85a6950ec3351f57b91c8b94a3f
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-02-26 20:28:37 +0900

    scrollImg

--- src/container/home/banner.jsx
+++ src/container/home/banner.jsx
@@ -6,19 +6,28 @@ const Banner = () => {
   const { link, DataUrl } = useBannerDetail();
   // console.log(DataUrl.allFile.edges);
   return (
-    <section className="relative w-full flex flex-col items-center">
-      <p className="absolute top-80 text-center z-10 font-bold text-7xl text-white break-keep">
-        실전 프로젝트를 통해 성장 기회와 프로로써의 경험을 얻을 수 있는 단체
-      </p>
+    <section className="relative">
       <img
-        className="bg-center  bg-cover w-full h-full align-middle  bg-no-repeat"
+        className="bg-center bg-cover w-full h-full align-middle bg-no-repeat"
         src={DataUrl}
-        alt=""
+        alt="BannerImg"
       />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center overflow-auto">
-        <span className="break-all mb-8 text-white text-6xl font-bold"></span>
-        <button className="bg-white text-black py-4 px-8 rounded-full">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <p className="text-2xl sm:text-4xl lg:text-7xl text-black font-bold mb-24 gap-1">
+          WANNABE 개발계의 AOMG
+        </p>
+        <button className=" bg-white sm:w-24 lg:w-36  font-bold py-4 px-8 rounded-full">
           <Link to={link}>지원하기</Link>
+        </button>
+      </div>
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-center w-full">
+        <button
+          onClick={() => {
+            window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+          }}
+          className=" font-bold py-4 px-8 text-4xl mb-20"
+        >
+          ⇩
         </button>
       </div>
     </section>
