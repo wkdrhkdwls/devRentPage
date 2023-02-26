8c6b75312cb3831f7817f150749142ff2d351aa2
george <hyunrim03@naver.com>
2023-02-26 14:43:04 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `devRentPage`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
